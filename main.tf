d56488f8a3002805c2fd7cd0d23a431bb1334485
Neeraj Saini <saini.ne@northeastern.edu>
2024-02-15 03:28:26 -0500

    Updated terraform files to allow for the creation of multiple VPC

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project     = "csye-6225-414318"
-  region      = var.region
+  project = "csye-6225-414318"
+  region  = var.region
 }
 
 # Create the VPC
 resource "google_compute_network" "vpc" {
-  name                    = "webapp-vpc"
+  name                    = "${var.vpc_name}-vpc"
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
   delete_default_routes_on_create = true
@@ -13,15 +13,15 @@ resource "google_compute_network" "vpc" {
 
 # Create the 'webapp' subnet in the VPC
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
+  name          = "${var.vpc_name}-webapp"
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
 }
 
-# Create the 'db' subnet in the VPC
+# Create the 'db' subnet in the VPCterraform apply -var "vpc_name=vpc1"
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
+  name          = "${var.vpc_name}-db"
   ip_cidr_range = "10.0.2.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
@@ -29,7 +29,7 @@ resource "google_compute_subnetwork" "db_subnet" {
 
 # Add a route to 0.0.0.0/0
 resource "google_compute_route" "webapp_internet_route" {
-  name             = "webapp-internet-route"
+  name             = "${var.vpc_name}-internet-route"
   dest_range       = "0.0.0.0/0"
   network          = google_compute_network.vpc.id
   next_hop_gateway = "default-internet-gateway"
