c0e63d414bc8f42c4bd30542552cca248bab3060
Neeraj Saini <saini.ne@northeastern.edu>
2024-02-15 02:14:49 -0500

    Main terraform and ci update

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  project     = "csye-6225-414318"
+  region      = var.region
+}
+
+# Create the VPC
+resource "google_compute_network" "vpc" {
+  name                    = "webapp-vpc"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+  delete_default_routes_on_create = true
+}
+
+# Create the 'webapp' subnet in the VPC
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name          = "webapp"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+# Create the 'db' subnet in the VPC
+resource "google_compute_subnetwork" "db_subnet" {
+  name          = "db"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+# Add a route to 0.0.0.0/0
+resource "google_compute_route" "webapp_internet_route" {
+  name             = "webapp-internet-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc.id
+  next_hop_gateway = "default-internet-gateway"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+# variables.tf
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+  default     = "us-east1"
+}
