c2ee6b079e3aa82fdb549043a8935aab18a04251
Neeraj Saini <saini.ne@northeastern.edu>
2024-02-21 11:53:29 -0500

    Added firewall rules and VM creation

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 provider "google" {
-  project = "csye-6225-414318"
+  project = var.project
   region  = var.region
 }
 
 # Create the VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.vpc_name}-vpc"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  name                       = "${var.vpc_name}-vpc"
+  auto_create_subnetworks    = false
+  routing_mode               = var.routing_mode
   delete_default_routes_on_create = true
 }
 
 # Create the 'webapp' subnet in the VPC
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "${var.vpc_name}-webapp"
-  ip_cidr_range = "10.0.1.0/24"
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.id
 }
 
 # Create the 'db' subnet in the VPCterraform apply -var "vpc_name=vpc1"
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "${var.vpc_name}-db"
-  ip_cidr_range = "10.0.2.0/24"
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.id
 }
@@ -33,4 +33,56 @@ resource "google_compute_route" "webapp_internet_route" {
   dest_range       = "0.0.0.0/0"
   network          = google_compute_network.vpc.id
   next_hop_gateway = "default-internet-gateway"
+}
+
+resource "google_compute_firewall" "allow_web_traffic" {
+  name    = "${var.vpc_name}-allow-web"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = var.protocol
+    ports    = var.webapp_port
+  }
+
+  source_ranges = [var.firewall_allow_source_ranges]
+}
+
+resource "google_compute_firewall" "deny_ssh" {
+  name    = "${var.vpc_name}-deny-ssh"
+  network = google_compute_network.vpc.id
+
+  deny {
+    protocol = var.protocol
+    ports    = var.ssh_port
+  }
+
+  source_ranges = [var.firewall_deny_source_ranges]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = ["web", "application"]
+  boot_disk {
+    initialize_params {
+      image = var.boot_disk_image
+      type  = var.boot_disk_type
+      size  = var.boot_disk_size
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.id
+    subnetwork = google_compute_subnetwork.webapp_subnet.id
+    access_config {
+      network_tier = var.vm_network_tier
+    }
+  }
+
+  service_account {
+    scopes = var.service_account_scopes
+  }
+
 }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,79 @@ variable "vpc_name" {
   description = "The name of the VPC to be created"
   type        = string
 }
+
+variable "project" {
+  description = "The GCP project ID."
+}
+
+variable "webapp_subnet_cidr" {
+  description = "CIDR block for the webapp subnet."
+  default     = "10.0.1.0/24"
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR block for the database subnet."
+  default     = "10.0.2.0/24"
+}
+
+variable "routing_mode" {
+  description = "The network routing mode (REGIONAL or GLOBAL)."
+  default     = "REGIONAL"
+}
+
+variable "webapp_subnet_name" {
+  default = "webapp"
+}
+
+variable "db_subnet_name" {
+  default = "db"
+}
+
+variable "webapp_port" {
+  default = "8080"
+}
+variable "instance_name" {
+
+}
+
+variable "machine_type" {
+  default = "e2-medium" 
+}
+
+variable "zone" {
+  default = "us-east1-b" 
+}
+
+variable "boot_disk_image" {
+
+}
+variable "boot_disk_type" {
+  default = "pd-balanced"
+}
+variable "boot_disk_size" {
+  default = 100
+}
+
+variable "service_account_scopes" {
+
+}
+
+variable "firewall_allow_source_ranges" {
+  
+}
+
+variable "firewall_deny_source_ranges" {
+  
+}
+
+variable "protocol" {
+  
+}
+
+variable "ssh_port" {
+  
+}
+
+variable "vm_network_tier" {
+  default = "PREMIUM"
+}
