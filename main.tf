e16adeaf75d7f31cb23f7e3b7cc08a5cc61f9e55
Neeraj Saini <saini.ne@northeastern.edu>
2024-02-25 23:17:26 -0500

    Updated main.tf to create cloud sql db and added startup script

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "google" {
 
 # Create the VPC
 resource "google_compute_network" "vpc" {
-  name                       = "${var.vpc_name}-vpc"
-  auto_create_subnetworks    = false
-  routing_mode               = var.routing_mode
+  name                            = "${var.vpc_name}-vpc"
+  auto_create_subnetworks         = false
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 }
 
@@ -30,9 +30,71 @@ resource "google_compute_subnetwork" "db_subnet" {
 # Add a route to 0.0.0.0/0
 resource "google_compute_route" "webapp_internet_route" {
   name             = "${var.vpc_name}-internet-route"
-  dest_range       = "0.0.0.0/0"
+  dest_range       = var.dest_range_webapp_internet_route
   network          = google_compute_network.vpc.id
-  next_hop_gateway = "default-internet-gateway"
+  next_hop_gateway = var.next_hop_gateway
+}
+
+# Reserve an IP range for the private services in the VPC
+resource "google_compute_global_address" "private_services_range" {
+  name          = "${var.vpc_name}-private-services-range"
+  purpose       = var.purpose_private_services_range
+  address_type  = var.address_type_private_services_range
+  network       = google_compute_network.vpc.self_link
+  address       = var.address_private_services_range
+  prefix_length = var.prefix_length_private_services_range
+}
+
+# Establish a private connection to Google services
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.id
+  service                 = var.service_networking_connection
+  reserved_peering_ranges = [google_compute_global_address.private_services_range.name]
+}
+
+resource "google_sql_database_instance" "mysql" {
+  name                = var.cloud_sql_instance_name
+  project             = var.project
+  region              = var.region
+  database_version    = var.database_version
+  deletion_protection = var.deletion_protection_enabled
+
+  settings {
+    tier              = var.tier_db
+    activation_policy = var.activation_policy_db
+    disk_autoresize   = var.disk_autoresize_db
+    backup_configuration {
+      enabled            = var.backup_configuration_enabled 
+      binary_log_enabled = var.binary_log_enabled_db
+    }
+    ip_configuration {
+      ipv4_enabled    = var.ipv4_enabled_db
+      private_network = google_compute_network.vpc.id
+    }
+    disk_type = var.disk_type_db
+    disk_size = var.disk_size_db
+
+    availability_type           = var.availability_type_db
+    deletion_protection_enabled = var.deletion_protection_enabled
+
+  }
+}
+
+resource "google_sql_database" "webapp_database" {
+  name     = var.webapp_db_name
+  instance = google_sql_database_instance.mysql.name
+}
+
+resource "random_password" "webapp_password" {
+  length           = 16
+  special          = true
+  override_special = var.override_special
+}
+
+resource "google_sql_user" "webapp_user" {
+  name     = var.webapp_user
+  instance = google_sql_database_instance.mysql.name
+  password = random_password.webapp_password.result
 }
 
 resource "google_compute_firewall" "allow_web_traffic" {
@@ -64,7 +126,7 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = var.machine_type
   zone         = var.zone
 
-  tags = ["web", "application"]
+  tags = var.tags
   boot_disk {
     initialize_params {
       image = var.boot_disk_image
@@ -84,5 +146,14 @@ resource "google_compute_instance" "vm_instance" {
   service_account {
     scopes = var.service_account_scopes
   }
+
+  metadata = {
+    db-user     = google_sql_user.webapp_user.name
+    db-password = google_sql_user.webapp_user.password
+    db-name     = google_sql_database.webapp_database.name
+    db-host     = google_sql_database_instance.mysql.private_ip_address
+  }
+
+  metadata_startup_script = file(var.startup_script_path)
 
 }
--- variables.tf
+++ variables.tf
@@ -46,11 +46,11 @@ variable "instance_name" {
 }
 
 variable "machine_type" {
-  default = "e2-medium" 
+  default = "e2-medium"
 }
 
 variable "zone" {
-  default = "us-east1-b" 
+  default = "us-east1-b"
 }
 
 variable "boot_disk_image" {
@@ -68,21 +68,174 @@ variable "service_account_scopes" {
 }
 
 variable "firewall_allow_source_ranges" {
-  
+
 }
 
 variable "firewall_deny_source_ranges" {
-  
+
 }
 
 variable "protocol" {
-  
+
 }
 
 variable "ssh_port" {
-  
+
 }
 
 variable "vm_network_tier" {
   default = "PREMIUM"
+}
+
+variable "cloud_sql_instance_name" {
+  description = "The name of the Cloud SQL instance"
+  type        = string
+}
+
+
+# Update the default to MySQL 8
+variable "database_version" {
+  description = "The database version to use."
+  type        = string
+  default     = "MYSQL_8_0"
+}
+
+variable "startup_script_path" {
+  type    = string
+  default = "./startup_script.sh"
+}
+
+variable "tags" {
+  description = "The tags to apply to the resources."
+  type        = list(string)
+  default     = ["web", "application"]
+}
+
+variable "webapp_user" {
+  description = "The username for the webapp database user."
+  type        = string
+  default = "webapp"
+  
+}
+
+variable "webapp_db_name" {
+  description = "The name of the webapp database."
+  type        = string
+  default     = "webapp"
+}
+
+variable "override_special" {
+  type        = string
+  default     = "!#$%&*()-_=+[]{}<>:?"
+}
+
+variable "availability_type_db" {
+  description = "The availability type for the Cloud SQL instance."
+  type        = string
+  default     = "REGIONAL"
+}
+
+variable "deletion_protection_enabled" {
+  description = "Whether deletion protection is enabled for the Cloud SQL instance."
+  type        = bool
+  default     = false
+}
+
+variable "disk_size_db" {
+  description = "The size of the disk for the Cloud SQL instance."
+  type        = number
+  default     = 100
+}
+
+variable "disk_type_db" {
+  description = "The type of disk for the Cloud SQL instance."
+  type        = string
+  default     = "PD_SSD"
+}
+
+variable "ipv4_enabled_db" {
+  description = "Whether the Cloud SQL instance has a public IP address."
+  type        = bool
+  default     = false
+  
+}
+
+variable "binary_log_enabled_db" {
+  description = "Whether binary logging is enabled for the Cloud SQL instance."
+  type        = bool
+  default     = true
+  
+}
+
+variable "backup_configuration_enabled" {
+  description = "Whether automated backups are enabled for the Cloud SQL instance."
+  type        = bool
+  default     = true
+  
+}
+
+variable "disk_autoresize_db" {
+  description = "Whether the disk for the Cloud SQL instance is automatically resized."
+  type        = bool
+  default     = true
+  
+}
+
+variable "activation_policy_db" {
+  description = "The activation policy for the Cloud SQL instance."
+  type        = string
+  default     = "ALWAYS"
+}
+
+variable "tier_db" {
+  description = "The machine type for the Cloud SQL instance."
+  type        = string
+  default     = "db-f1-micro"
+  
+}
+
+variable "service_networking_connection" {
+  description = "The name of the service networking connection."
+  type        = string
+  default     = "servicenetworking.googleapis.com"
+}
+
+variable "purpose_private_services_range" {
+  description = "The purpose of the private services range."
+  type        = string
+  default     = "VPC_PEERING"
+  
+}
+
+variable "address_type_private_services_range" {
+  description = "The address type of the private services range."
+  type        = string
+  default     = "INTERNAL"
+  
+}
+
+variable "address_private_services_range" {
+  description = "The address of the private services range."
+  type        = string
+  default     = "10.0.3.0"
+}
+
+variable "prefix_length_private_services_range" {
+  description = "The prefix length of the private services range."
+  type        = number
+  default     = 24
+}
+
+variable "next_hop_gateway" {
+  description = "The next hop gateway for the route."
+  type        = string
+  default     = "default-internet-gateway"
+  
+}
+
+variable "dest_range_webapp_internet_route" {
+  description = "The destination range for the webapp internet route."
+  type        = string
+  default     = "0.0.0.0/0"
+  
 }
